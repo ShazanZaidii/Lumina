793480fb3a4338465cbbd48c5f8d67937b3677f6
Shazan Zaidi <rit609009@gmail.com>
2025-11-12 01:00:55 +0530

    Initial Commit

---
+++ Lumina/LuminaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuminaApp.swift
+//  Lumina
+//
+//  Created by Shazan Zaidi on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct LuminaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
